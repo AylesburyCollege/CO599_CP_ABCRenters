ea27fb6af588bc3ba7d8095bab05962e387ac30a
DerekPeacock <derek.peacock@yahoo.com>
2019-05-16 19:52:17 +0100

    Moved controllers to the correct folder, added a new shared layout for the CRUD pages.  Properties still a problem

--- CO599_CP_ABCRenters/Controllers/AddressesController.cs
+++ CO599_CP_ABCRenters/Controllers/AddressesController.cs
@@ -6,8 +6,9 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using CO599_CP_ABCRenters.Models;
 
-namespace CO599_CP_ABCRenters.Models
+namespace CO599_CP_ABCRenters.Controllers
 {
     public class AddressesController : Controller
     {
